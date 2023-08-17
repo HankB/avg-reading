d146bab852773689825e27b7879af697bedeb9e4
Hank Barta <hbarta@gmail.com>
2023-08-16 19:30:58 -0500

    handle missing fields

--- avg_reading.py
+++ avg_reading.py
@@ -8,6 +8,10 @@ def parse_line(line):
     """ parse timestamp and temperature from input """
     try:
         fields = json.loads(line)
+        # do the fields exist in the JSON?
+        if "t" not in fields or "temp" not in fields:
+            return (-1, "")
+    # Error parsing JSON
     except ValueError:
         print("Cannot decode '{}'".format(line.strip("\n")), file=sys.stderr)
         return (-1, "")
